45fa78c16e4711338611c726d3a8c5e0ba5dedd1
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-05-01 09:44:08 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const express = require('express'); // import express js module 
 const app = express(); // Create new express app instance
 
+const crypto = require('crypto'); // node function to generate UUID
+
 app.use(express.json()); // middleware function to parse incoming JSON payload
 
 const products = [
-  {
-    name: "laptop",
-    price: "$500.00",
-    quantity: 4,
-    active: true
-  },
-  {
-    name: "keyboard",
-    price: "$45.00",
-    quantity: 15,
-    active: true
-  }
+    {
+        "id": "a5589568-6a40-44c9-91a5-ad50b993efd5",
+        "name": "laptop",
+        "price": "$500.00",
+        "quantity": 4,
+        "active": true
+    },
+    {
+        "id": "a5589568-6a40-44c9-91a5-ad50b993efd6",
+        "name": "keyboard",
+        "price": "$45.00",
+        "quantity": 15,
+        "active": true
+    },
+    {
+        "id": "a5589568-6a40-44c9-91a5-ad50b993efd7",
+        "name": "computer",
+        "price": "$1000",
+        "quantity": 6,
+        "active": true
+    }
 ]
 
-app.get('/', (request, response)=>{
+app.get('/', (request, response) => {
   response.send('Testing...');
 })
 
@@ -26,17 +37,31 @@ app.get('/products', (request, response) => {
   response.json(products);
 })
 
-app.post('/products', (request, response)=>{
+app.post('/products', (request, response) => {
   const { name, price, quantity, active } = request.body
 
+  // check if request contains a name, if not 422 req
+  if(!name) {
+    return response.status(422).json({ message: "name is required" });
+  }
+
+  const id = crypto.randomUUID();
+
   products.push({
+    id,
     name,
     price,
     quantity,
     active
   })
 
-  response.status(201).json({ message: "product created successfully" });
+  response.status(201).json({ message: "product created successfully", id });
+})
+
+app.get('/products/:id', (request, response) => {
+  const product = products.find(product => product.id == request.params.id)
+
+  response.json(product)
 })
 
 app.listen('3000', () => console.log('server started on port 3000'));
